c7c26ec941bebac83fdab272be9c035f129091f5
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2026-05-28 23:00:23 -0300

    test: update test_create_db to accept session parameter and add user to session

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,6 +1,8 @@
 from src.api_gerenciador_tarefas.models.user_schema import User
 
-def test_create_db():
+def test_create_db(session):
     user = User(user_name="ronald", email="ererer",password="deus")
-
+    breakpoint()
+    session.add(user)
+    session.commit()
     assert user.password == "deus"
