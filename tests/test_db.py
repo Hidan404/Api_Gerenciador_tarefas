fd34d82c55332e32a549e8517fa93632c1f18f13
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2026-06-02 18:28:15 -0300

    refactor: remove unused compiled Python files and test scripts

--- tests/test_db.py
+++
@@ -1,8 +0,0 @@
-from src.api_gerenciador_tarefas.models.user_schema import User
-
-def test_create_db(session):
-    user = User(user_name="ronald", email="ererer",password="deus")
-    breakpoint()
-    session.add(user)
-    session.commit()
-    assert user.password == "deus"
